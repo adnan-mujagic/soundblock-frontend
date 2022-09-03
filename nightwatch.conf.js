d8d5c0c79be3d81d6d82e6f50a381eab1c946757
adnan-mujagic <adnanmujagic@outlook.com>
2022-09-04 00:06:21 +0200

    Cleaning up some redundant templates

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -14,51 +14,50 @@
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
-  src_folders: ['tests/specs'],
+  src_folders: ["tests/specs"],
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
-  page_objects_path: ['tests/page-objects'],
+  page_objects_path: ["tests/page-objects"],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
-  custom_commands_path: ['tests/custom-commands'],
+  custom_commands_path: ["tests/custom-commands"],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
-  custom_assertions_path: ['tests/custom-assertions'],
+  custom_assertions_path: ["tests/custom-assertions"],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
   // plugins: [],
-  
+
   // See https://nightwatchjs.org/guide/concepts/test-globals.html
-  globals_path: '',
+  globals_path: "",
 
   webdriver: {},
 
   test_settings: {
     default: {
       disable_error_log: false,
-      launch_url: 'http://localhost:3000',
+      launch_url: "${REACT_APP_PREVIEW_URL}",
 
       screenshots: {
         enabled: false,
-        path: 'screens',
-        on_failure: true
+        path: "screens",
+        on_failure: true,
       },
 
       desiredCapabilities: {
-        browserName: 'chrome'
+        browserName: "chrome",
       },
-      
+
       webdriver: {
         start_process: true,
-        server_path: ''
+        server_path: "",
       },
-      
     },
-    
+
     chrome: {
       desiredCapabilities: {
-        browserName: 'chrome',
-        'goog:chromeOptions': {
+        browserName: "chrome",
+        "goog:chromeOptions": {
           // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
           //
           // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
@@ -68,18 +67,17 @@ module.exports = {
             //'--ignore-certificate-errors',
             //'--allow-insecure-localhost',
             //'--headless'
-          ]
-        }
+          ],
+        },
       },
 
       webdriver: {
         start_process: true,
-        server_path: '',
+        server_path: "",
         cli_args: [
           // --verbose
-        ]
-      }
+        ],
+      },
     },
-    
-  }
+  },
 };
--- tests/specs/basic/landingPage.js
+++ tests/specs/basic/landingPage.js
@@ -1,8 +1,6 @@
 describe("Landing page", function () {
-  before((browser) => browser.navigateTo("http://localhost:3000"));
-
   it("Testing landing page", function (browser) {
-    browser.waitForElementVisible("body").assert.titleContains("Rattle");
+    browser.init().waitForElementVisible("body").assert.titleContains("Rattle");
   });
 
   after((browser) => browser.end());
